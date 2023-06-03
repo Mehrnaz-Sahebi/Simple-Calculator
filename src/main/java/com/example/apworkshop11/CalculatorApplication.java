7bd9fccf80970455c0337a00f877bc44c6bc2b71
Mehrnaz Sahebi <mehrnazsa83@gmail.com>
2023-06-03 23:21:09 +0330

    feat  : buttons

--- src/main/java/com/example/apworkshop11/CalculatorApplication.java
+++ src/main/java/com/example/apworkshop11/CalculatorApplication.java
@@ -2,6 +2,7 @@ package com.example.apworkshop11;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,10 +10,15 @@ public class CalculatorApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+//        Parent root = FXMLLoader.load(getClass().getResource("calculator.fxml"));
         FXMLLoader fxmlLoader = new FXMLLoader(CalculatorApplication.class.getResource("calculator.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("AP-Calculator");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
--- src/main/java/com/example/apworkshop11/Controler.java
+++ src/main/java/com/example/apworkshop11/Controler.java
@@ -1,4 +1,75 @@
 package com.example.apworkshop11;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class Controler {
+    private StringBuilder stringToCalculate = new StringBuilder();
+    @FXML
+    public void addOne(ActionEvent actionEvent){
+        stringToCalculate.append("1");
+    }
+    public void addTwo(ActionEvent actionEvent){
+        stringToCalculate.append("2");
+    }
+    @FXML
+    public void addThree(ActionEvent actionEvent){
+        stringToCalculate.append("3");
+    }
+    @FXML
+    public void addFour(ActionEvent actionEvent){
+        stringToCalculate.append("4");
+    }
+    @FXML
+    public void addFive(ActionEvent actionEvent){
+        stringToCalculate.append("5");
+    }
+    @FXML
+    public void addSix(ActionEvent actionEvent){
+        stringToCalculate.append("6");
+    }
+    @FXML
+    public void addSeven(ActionEvent actionEvent){
+        stringToCalculate.append("7");
+    }
+    @FXML
+    public void addEight(ActionEvent actionEvent){
+        stringToCalculate.append("8");
+    }
+    @FXML
+    public void addNine(ActionEvent actionEvent){
+        stringToCalculate.append("9");
+    }
+    @FXML
+    public void addZero(ActionEvent actionEvent){
+        stringToCalculate.append("0");
+    }
+    @FXML
+    public void addPlus(ActionEvent actionEvent){
+        stringToCalculate.append("+");
+    }
+    @FXML
+    public void addMinus(ActionEvent actionEvent){
+        stringToCalculate.append("-");
+    }
+    @FXML
+    public void addMultiply(ActionEvent actionEvent){
+        stringToCalculate.append("*");
+    }
+    @FXML
+    public void addDivide(ActionEvent actionEvent){
+        stringToCalculate.append("/");
+    }
+    @FXML
+    public void addPoint(ActionEvent actionEvent) {
+        stringToCalculate.append(".");
+    }
+    @FXML
+    public void equals(ActionEvent actionEvent){
+
+    }
+    @FXML
+    public void ac(ActionEvent actionEvent){
+
+    }
 }
