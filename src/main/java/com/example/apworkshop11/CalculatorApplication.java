f3713c906542856cbdffcac9ef05e5f6460019aa
Mehrnaz Sahebi <mehrnazsa83@gmail.com>
2023-06-04 12:30:50 +0330

    feat : textfield

--- src/main/java/com/example/apworkshop11/CalculatorApplication.java
+++ src/main/java/com/example/apworkshop11/CalculatorApplication.java
@@ -1,9 +1,12 @@
 package com.example.apworkshop11;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class CalculatorApplication extends Application {
@@ -14,6 +17,47 @@ public class CalculatorApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(CalculatorApplication.class.getResource("calculator.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("AP-Calculator");
+        Controler controler = fxmlLoader.getController();
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if(event.getCode() == KeyCode.DIGIT0 || event.getCode()==KeyCode.NUMPAD0){
+                    controler.addZero();
+                } else if (event.getCode() == KeyCode.DIGIT1 || event.getCode()==KeyCode.NUMPAD1) {
+                    controler.addOne();
+                } else if(event.getCode() == KeyCode.DIGIT2 || event.getCode()==KeyCode.NUMPAD2){
+                    controler.addTwo();
+                } else if (event.getCode() == KeyCode.DIGIT3 || event.getCode()==KeyCode.NUMPAD3) {
+                    controler.addThree();
+                } else if (event.getCode() == KeyCode.DIGIT4 || event.getCode()==KeyCode.NUMPAD4) {
+                    controler.addFour();
+                } else if (event.getCode() == KeyCode.DIGIT5 || event.getCode()==KeyCode.NUMPAD5) {
+                    controler.addFive();
+                }  else if (event.getCode() == KeyCode.DIGIT6 || event.getCode()==KeyCode.NUMPAD6) {
+                    controler.addSix();
+                } else if (event.getCode() == KeyCode.DIGIT7 || event.getCode()==KeyCode.NUMPAD7) {
+                    controler.addSeven();
+                } else if (event.getCode() == KeyCode.DIGIT8 || event.getCode()==KeyCode.NUMPAD8) {
+                    controler.addEight();
+                } else if (event.getCode() == KeyCode.DIGIT9 || event.getCode()==KeyCode.NUMPAD9) {
+                    controler.addNine();
+                } else if (event.getCode() == KeyCode.ADD) {
+                    controler.addPlus();
+                } else if (event.getCode() == KeyCode.SUBTRACT) {
+                    controler.addMinus();
+                } else if (event.getCode() == KeyCode.MULTIPLY) {
+                    controler.addMultiply();
+                } else if (event.getCode() == KeyCode.DIVIDE) {
+                    controler.addDivide();
+                } else if (event.getCode() == KeyCode.DECIMAL) {
+                    controler.addPoint();
+                } else if (event.getCode() == KeyCode.EQUALS || event.getCode() == KeyCode.ENTER) {
+                    controler.equals();
+                } else if(event.getCode() == KeyCode.BACK_SPACE) {
+                    controler.backSpace();
+                }
+            }
+        });
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/apworkshop11/Controler.java
+++ src/main/java/com/example/apworkshop11/Controler.java
@@ -2,74 +2,177 @@ package com.example.apworkshop11;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class Controler {
-    private StringBuilder stringToCalculate = new StringBuilder();
+    private static StringBuilder stringToCalculate = new StringBuilder();
+    @FXML
+    private TextField textField = new TextField();
     @FXML
     public void addOne(ActionEvent actionEvent){
         stringToCalculate.append("1");
+        textField.setText(stringToCalculate.toString());
     }
+    @FXML
+    public void addOne(){
+        stringToCalculate.append("1");
+        textField.setText(stringToCalculate.toString());
+    }
+    @FXML
     public void addTwo(ActionEvent actionEvent){
         stringToCalculate.append("2");
+        textField.setText(stringToCalculate.toString());
+    }
+    @FXML
+    public void addTwo(){
+        stringToCalculate.append("2");
+        textField.setText(stringToCalculate.toString());
     }
     @FXML
     public void addThree(ActionEvent actionEvent){
         stringToCalculate.append("3");
+        textField.setText(stringToCalculate.toString());
+    }
+    @FXML
+    public void addThree(){
+        stringToCalculate.append("3");
+        textField.setText(stringToCalculate.toString());
     }
     @FXML
     public void addFour(ActionEvent actionEvent){
         stringToCalculate.append("4");
+        textField.setText(stringToCalculate.toString());
+    }
+    @FXML
+    public void addFour(){
+        stringToCalculate.append("4");
+        textField.setText(stringToCalculate.toString());
     }
     @FXML
     public void addFive(ActionEvent actionEvent){
         stringToCalculate.append("5");
+        textField.setText(stringToCalculate.toString());
+    }
+    @FXML
+    public void addFive(){
+        stringToCalculate.append("5");
+        textField.setText(stringToCalculate.toString());
     }
     @FXML
     public void addSix(ActionEvent actionEvent){
         stringToCalculate.append("6");
+        textField.setText(stringToCalculate.toString());
+    }
+    @FXML
+    public void addSix(){
+        stringToCalculate.append("6");
+        textField.setText(stringToCalculate.toString());
     }
     @FXML
     public void addSeven(ActionEvent actionEvent){
         stringToCalculate.append("7");
+        textField.setText(stringToCalculate.toString());
+    }
+    @FXML
+    public void addSeven(){
+        stringToCalculate.append("7");
+        textField.setText(stringToCalculate.toString());
     }
     @FXML
     public void addEight(ActionEvent actionEvent){
         stringToCalculate.append("8");
+        textField.setText(stringToCalculate.toString());
+    }
+    @FXML
+    public void addEight(){
+        stringToCalculate.append("8");
+        textField.setText(stringToCalculate.toString());
     }
     @FXML
     public void addNine(ActionEvent actionEvent){
         stringToCalculate.append("9");
+        textField.setText(stringToCalculate.toString());
+    }
+    @FXML
+    public void addNine(){
+        stringToCalculate.append("9");
+        textField.setText(stringToCalculate.toString());
     }
     @FXML
     public void addZero(ActionEvent actionEvent){
         stringToCalculate.append("0");
+        textField.setText(stringToCalculate.toString());
+    }
+    public void addZero(){
+        stringToCalculate.append("0");
+        textField.setText(stringToCalculate.toString());
     }
     @FXML
     public void addPlus(ActionEvent actionEvent){
         stringToCalculate.append("+");
+        textField.setText(stringToCalculate.toString());
+    }
+    @FXML
+    public void addPlus(){
+        stringToCalculate.append("+");
+        textField.setText(stringToCalculate.toString());
     }
     @FXML
     public void addMinus(ActionEvent actionEvent){
         stringToCalculate.append("-");
+        textField.setText(stringToCalculate.toString());
+    }
+    @FXML
+    public void addMinus(){
+        stringToCalculate.append("-");
+        textField.setText(stringToCalculate.toString());
     }
     @FXML
     public void addMultiply(ActionEvent actionEvent){
         stringToCalculate.append("*");
+        textField.setText(stringToCalculate.toString());
+    }
+    @FXML
+    public void addMultiply(){
+        stringToCalculate.append("*");
+        textField.setText(stringToCalculate.toString());
     }
     @FXML
     public void addDivide(ActionEvent actionEvent){
         stringToCalculate.append("/");
+        textField.setText(stringToCalculate.toString());
+    }
+    @FXML
+    public void addDivide(){
+        stringToCalculate.append("/");
+        textField.setText(stringToCalculate.toString());
     }
     @FXML
     public void addPoint(ActionEvent actionEvent) {
         stringToCalculate.append(".");
+        textField.setText(stringToCalculate.toString());
+    }
+    @FXML
+    public void addPoint() {
+        stringToCalculate.append(".");
+        textField.setText(stringToCalculate.toString());
     }
     @FXML
     public void equals(ActionEvent actionEvent){
 
     }
     @FXML
-    public void ac(ActionEvent actionEvent){
+    public void equals(){
 
+    }
+    @FXML
+    public void ac(ActionEvent actionEvent){
+        stringToCalculate.delete(0,stringToCalculate.length());
+        textField.setText(stringToCalculate.toString());
+    }
+    @FXML
+    public void backSpace(){
+        stringToCalculate.delete(stringToCalculate.length()-1,stringToCalculate.length());
+        textField.setText(stringToCalculate.toString());
     }
 }
